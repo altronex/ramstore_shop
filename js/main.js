2a2d16cfd2fee3d428216298620b6edb8b77c409
neoxa <romanunited@yadnex.ru>
2020-10-17 17:58:04 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function () {
+
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/arrow-right.svg" alt=""></button>',
+  });
+
+  $('.tab').on('click', function(e){
+      e.preventDefault();
+
+      $($(this).siblings()).removeClass('tab--active');
+      $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+
+      $(this).addClass('tab--active');
+      $($(this).attr('href')).addClass('tabs-content--active');
+  });
+
+  $('.product-item__favourite').on('click', function(){
+    $(this).toggleClass('product-item__favourite--active')
+  })
+
+  $('.product-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="img/arrow-black-left.svg" alt=""></button>',
+    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="img/arrow-black-right.svg" alt=""></button>',
+  });
+
+  $('.filter-style').styler();
+
+  $('.filter__item-drop, .filter__extra').on('click', function(){
+    $(this).toggleClass('filter__item-drop--active');
+    $(this).next().slideToggle('200');
+  });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 100000,
+    max: 500000,
+  });
+  
+  $('.catalog__filters-btngrid').on('click', function(){
+    $(this).addClass('catalog__filters-button--active');
+    $('.catalog__filters-btnline').removeClass('catalog__filters-button--active');
+    $('.product-item__wrapper').removeClass('product-item__wrapper--list');
+  });
+  
+  $('.catalog__filters-btnline').on('click', function(){
+    $(this).addClass('catalog__filters-button--active');
+    $('.catalog__filters-btngrid').removeClass('catalog__filters-button--active');
+    $('.product-item__wrapper').addClass('product-item__wrapper--list');
+  });
+});
